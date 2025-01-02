e63ae9fdac5192a2d75d0efb9bc4855fca2ca132
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2025-01-02 20:22:28 +0530

    ..

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -11,8 +11,8 @@ function Freebook() {
     const getBook = async () => {
       try {
         const res = await axios.get("http://localhost:4001/book");
-        console.log(res.data);
         const data = res.data.filter((data) => data.category === "free");
+        console.log(data);
         setBook(data);
       } catch (error) {
         console.log("Error: ", error);
